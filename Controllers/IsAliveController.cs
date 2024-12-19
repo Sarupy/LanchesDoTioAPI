70734996795233cc8f8ef5ce171df8632c38df64
Caíque Santos <kaikysarupy@hotmail.com>
2024-12-18 23:16:24 -0300

    is alive

---
+++ Controllers/IsAliveController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesDoTioAPI.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class IsAliveController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Object> Get()
+        {
+            return Ok(new { status = "Alive!" });
+        }
+    }
+}
